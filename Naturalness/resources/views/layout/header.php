27383f1322f459ea4cbff89931b5b1ececb752eb
Coryse <coryse.quibel@gmail.com>
2020-01-14 12:46:23 +0100

    Suppression fichier inutile

--- Naturalness/resources/views/layout/header.php
+++ Naturalness/resources/views/layout/header.php
@@ -16,15 +16,12 @@
 
   <title>Naturalness</title>
 </head>
-
 <body>
-
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="<?= route('home'); ?>">Accueil</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarColor01">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
@@ -36,10 +33,8 @@
         <li class="nav-item">
           <a class="nav-link" href="<?= route('material'); ?>">Matériel </a>
         </li>
-
         <!-- If user not connected -->
         <?php if (!$userIsConnected) : ?>
-
           <li class="nav-item">
             <a class="nav-link" href="<?= route('signup'); ?>">Inscription</a></li>
           <li class="nav-item">
@@ -55,16 +50,12 @@
           <a class="nav-link" href="<?= route('about'); ?>">About</a>
         </li>
       </ul>
-
       <!-- Search engine -->
       <!-- in get so as the search can be shared by the user-->
       <form class="form-inline my-2 my-lg-0" method="get" action="<?= route('search'); ?>">
         <input class="form-control mr-sm-2" type="text" name="q" placeholder="Search">
         <button class="btn my-2 my-sm-0" type="submit">Recherche</button>
       </form>
-
-
     </div>
   </nav>
-
   <main>
